7b66055923816b1c2ebd7f122a14b9df92060b40
PikaYeti <166683154+PikaYeti@users.noreply.github.com>
2024-08-22 20:38:35 +0900

    Add files via upload

--- Main_1074_김가연.java
+++ Main_1074_김가연.java
@@ -1,4 +1,4 @@
-package D_0821;
+package SWEA;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,37 +32,31 @@ public class Main_1074_김가연 {
 		cnt = 0;
 		
 		
-		// half가 2일때 까지 while문 돌려주기
+
 		// 좌표는 2 -> 1-> 3 -> 4 사분면 순서대로 채워짐
 		while (half >= 2) {
-			// 분할정복 하기 위하여 half를 2로 나눠주기
+			// 분할정복 half 계속 나눠주기
 			half = half / 2;
-			// row
-			// 만약 좌표의 x좌표가 1, 2 사분면에 속해있다면
+			// 만약 좌표가 1, 2 사분면에 속해있다면
 			if ((row <= r) && (r < (row + half))) { 
-				// row, col
 				// 2사분면
 				if ((col <= c) && (c < (col + half))) { 
 					row = row;
 					col = col;
-				// row, col + half
-				// 1사분면 ->
+				// 1사분면 -> 2사분면이 채워지고 왔기 때문에 2사분면 좌표 수 만큼 cnt에 더해줌
 				} else { 
 					cnt += half * half;
 					row = row;
 					col = col + half;
 				}
-			// 만약 좌표의 x좌표가 3, 4 사분면에 속해있다면
-			// row + half
+			// 만약 좌표가 3, 4분면에 속해있다면
 			} else { 
-				// row + half, col
-				// 3사분면
+				// 3사분면 -> 1, 2 사분면이 채워지고 왔기 때문에 1, 2사분면 좌표수만큼 더해줌
 				if ((col <= c) && (c < (col + half))) { 
 					cnt += (half * half) * 2;
 					row = row + half;
 					col = col;
-				// row + half, col + half	
-				// 4사분면
+				// 4사분면 -> 1, 2, 3 채워지고 왔기 때문에 1, 2,3 좌표수 +
 				} else { 
 					cnt += (half * half) * 3;
 					row = row + half;
@@ -71,16 +65,7 @@ public class Main_1074_김가연 {
 			}
 		}
 		
-		for (int i = row ; i < (row + half) ; i++) {
-			for (int j = col ; j < (col + half) ; j++) {
-				if ((i == r) && (j == c)) {
-					break;
-				} else {
-					cnt += 1;
-				}
-			}
-		}
-		
+		// 더한 이동 수 찍어주기
 		System.out.println(cnt);
 	}
 
