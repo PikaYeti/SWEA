119d44873bef26b6a6e884ba6fd8780bb10f5f40
PikaYeti <166683154+PikaYeti@users.noreply.github.com>
2024-09-02 03:35:21 +0900

    Add files via upload

---
+++ 월말/순열.java
@@ -0,0 +1,52 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 순열 {
+	
+	static int n;
+	static BufferedWriter bw;
+	
+	static void permu(boolean [] visit, int [] arr, int depth, int r) throws IOException {
+		
+		if (depth == r) {
+			for (int i = 0 ; i < arr.length ; i++) {
+				System.out.println(arr[i]);
+			}
+			return;
+		}
+		
+		for (int i = 0 ; i < n ; i ++) {
+			if(!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				permu(visit, arr, depth + 1, r);
+				visit[i] = false;
+			}
+		}
+		return;
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		boolean [] visit = new boolean[n];
+		
+		int s = Integer.parseInt(st.nextToken());
+		int [] arr = new int [s];
+		
+		permu(visit, arr, 0, s);
+
+	}
+
+}
